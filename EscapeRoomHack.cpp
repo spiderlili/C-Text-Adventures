db4a495ebd52a4cccd8249ad8de3abecae5b549f
Jing Tan <jtan1993@gmail.com>
2021-11-29 18:52:58 +0000

    debug

--- EscapeRoomHack.cpp
+++ EscapeRoomHack.cpp
@@ -1,3 +1,18 @@
+#include <iostream>
+
 int main(){
+    std::cout << "You are a secret agent breaking into a secure server room";
+    std::cout << std::endl; 
+    std::cout << "You need to enter the correct codes to continue..";
+
+    int a = 4;
+    int b = 3;
+    int c = 2;
+    int sum = a + b + c;
+    int multipliedProduct = a * b * c;
+
+    std::cout << std::endl;
+    std::cout << multipliedProduct << std::endl;
+    std::cout << sum << std::endl;
     return 0;
 }
